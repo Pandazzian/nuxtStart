04ca1b88dace1de07d890beb6beb4f990f5d17ed
PanDaZZiaN <gem@autopair.co>
2021-04-05 19:36:00 +0700

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="@/assets/Gawr_Gura_Portrait.png">
+      <h1 class="title">
+        Pongsawut
+      </h1>
+      <div>{{text}}</div>  
+      <div> Hello,World! </div>
+      <div class="links">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+          @click="setText('ABC')"
+        >
+          change text
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted(){
+    this.setText("Hello");
+  },
+  data(){
+    return {
+      text:''
+    }
+  },
+  methods:{
+    setText(t){
+      this.text = t
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
